80e7149392ffd45031443f5f7e86c48eae7e710c
三苟 Sango <letusgoone@outlook.com>
2024-11-26 23:27:07 +0800

    添加项目文件。

---
+++ include/cango/result_code.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <compare>
+
+namespace cango {
+    /// @brief 代表可能异常的函数返回的异常代码，使用 @c int 表示异常代码
+    struct result_code {
+        static constexpr auto success_code = 0;
+
+        /// @brief 代表错误代码
+        int code;
+
+        /// @brief 检查错误代码是否代表结果为失败，无法处理错误
+        [[nodiscard]] constexpr bool is_failed() const noexcept { return code < success_code; }
+
+        /// @brief 检查错误代码是否代表结果为成功或警告，中间的错误有可能被掩盖
+        [[nodiscard]] constexpr bool is_not_failed() const noexcept { return code >= success_code; }
+
+        /// @brief 检查错误代码是否代表结果为警告，有被处理的错误，需要引起注意
+        [[nodiscard]] constexpr bool is_warned() const noexcept { return code > success_code; }
+
+        /// @brief 检查错误代码是否代表结果为成功，没有错误
+        [[nodiscard]] constexpr bool is_success() const noexcept { return code == success_code; }
+
+        /// @brief 利用错误代码中的 @c code 比较大小
+        friend std::strong_ordering operator<=>(const result_code& lhs, const result_code& rhs) noexcept {
+            return lhs.code <=> rhs.code;
+        }
+
+        /// @brief 转换为 @c int
+        constexpr operator int() const noexcept { return code; }
+    };
+}
